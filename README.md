32ad9f36b16bef0480ce480e0233853ce68f6394
Blake Embrey <hello@blakeembrey.com>
2016-02-06 10:10:19 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Typed Rimraf
+
+The type definition for [`rimraf`](https://github.com/isaacs/rimraf).
+
+## License
+
+MIT
---
+++ rimraf.d.ts
@@ -0,0 +1,30 @@
+import glob = require('glob');
+import { unlink, chmod, stat, lstat, rmdir, readdir, unlinkSync, chmodSync, statSync, lstatSync, rmdirSync, readdirSync } from 'fs';
+
+declare function rimraf (pattern: string, callback: (error: Error) => any): void;
+declare function rimraf (pattern: string, options: rimraf.Options, callback: (error: Error) => any): void;
+
+declare namespace rimraf {
+  export interface Options {
+    unlink?: typeof unlink;
+    chmod?: typeof chmod;
+    stat?: typeof stat;
+    lstat?: typeof lstat;
+    rmdir?: typeof rmdir;
+    readdir?: typeof readdir;
+    unlinkSync?: typeof unlinkSync;
+    chmodSync?: typeof chmodSync;
+    statSync?: typeof statSync;
+    lstatSync?: typeof lstatSync;
+    rmdirSync?: typeof rmdirSync;
+    readdirSync?: typeof readdirSync;
+    maxBusyTries: number;
+    emfileWait: number;
+    glob: boolean | glob.Options;
+    disableGlob: boolean;
+  }
+
+  export function sync (pattern: string, options?: rimraf.Options): void;
+}
+
+export = rimraf;
